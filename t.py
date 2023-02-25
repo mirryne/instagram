dd774913c1565daac295e7d5e59bfa101c0fc0de
Future Player <78831803+mirryne@users.noreply.github.com>
2023-02-25 23:37:30 +0900

    nav rejuran searching machine(proto 1)

---
+++ t.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+ 
+
+파일 = open ('rejuran-naver.txt','w')
+
+# print(파일.write("1페이지" + '\n'))
+data = requests.get('https://search.naver.com/search.naver?display=15&f=&filetype=0&page=2&query=%EB%A6%AC%EC%A5%AC%EB%9E%80%ED%9E%90%EB%9F%AC&research_url=&sm=tab_pge&start=1&where=web')
+ 
+soup = BeautifulSoup(data.text.replace('\\',''),'html.parser')
+ 
+글리스트 = soup.select('a.link_tit')
+
+# 파일 = open ('rejuran-naver.txt','w')
+
+print(파일.write("1페이지" + '\n'))
+for i in range(15) :
+    print(파일.write(글리스트[i]['href'] + '\n'))
