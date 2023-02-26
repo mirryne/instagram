063cb98b82bf04b6aa85218f366088b6567cd363
Future Player <78831803+mirryne@users.noreply.github.com>
2023-02-26 22:48:40 +0900

    7

--- t.py
+++ t.py
@@ -6,7 +6,9 @@ import time
 파일 = open ('rejuran-naver.txt','w')
 
 # print(파일.write("1페이지" + '\n'))
-data = requests.get('https://search.naver.com/search.naver?display=15&f=&filetype=0&page=2&query=%EB%A6%AC%EC%A5%AC%EB%9E%80%ED%9E%90%EB%9F%AC&research_url=&sm=tab_pge&start=1&where=web')
+
+k = '리쥬란힐러'
+data = requests.get('https://search.naver.com/search.naver?display=15&f=&filetype=0&page=2&query=[k]&research_url=&sm=tab_pge&start=1&where=web')
  
 soup = BeautifulSoup(data.text.replace('\\',''),'html.parser')
  
@@ -17,3 +19,5 @@ soup = BeautifulSoup(data.text.replace('\\',''),'html.parser')
 print(파일.write("1페이지" + '\n'))
 for i in range(15) :
     print(파일.write(글리스트[i]['href'] + '\n'))
+
+    https://search.naver.com/search.naver?display=15&f=&filetype=0&page=2&query=%EB%A6%AC%EC%A5%AC%EB%9E%80%ED%9E%90%EB%9F%AC&research_url=&sm=tab_pge&start=1&where=web
